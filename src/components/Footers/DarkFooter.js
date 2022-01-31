a1dd38d294b943720a2bb1faf92e49908c6fabcc
arakhmatov <arakhmatov@ngxinteractive.com>
2022-01-31 05:32:41 -0800

    minor updates to footer credits and copyright

--- src/components/Footers/DarkFooter.js
+++ src/components/Footers/DarkFooter.js
@@ -11,21 +11,9 @@ function DarkFooter() {
         <nav>
         </nav>
         <div className="copyright" id="copyright">
-          © {new Date().getFullYear()}, Designed by{" "}
-          <a
-            href="https://www.invisionapp.com?ref=nukr-dark-footer"
-            target="_blank"
-          >
-            Invision
-          </a>
-          . Coded by{" "}
-          <a
-            href="https://www.creative-tim.com?ref=nukr-dark-footer"
-            target="_blank"
-          >
-            Anushervon Rakhmatov
-          </a>
-          .
+         
+          {`Dubai, UAE. Globalcot © ${new Date().getFullYear()}`}
+         
         </div>
       </Container>
     </footer>
